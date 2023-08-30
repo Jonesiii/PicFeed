adc2a0afb6ec16ef723aacc48fbc0e8d5553e8d8
Joonas Arola <joonas.arola@tuni.fi>
2023-08-30 16:37:49 +0300

    add basic layout to frontend

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,55 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import Searchbar from './components/Searchbar.vue';
 
 </script>
 
 <template>
-  <div>
-    <nav>
-        <RouterLink to="/">Home</RouterLink>
+  <div class="container">
+    <nav class="navigation">
+      <RouterLink to="/">Home</RouterLink>
     </nav>
+    <div class="content-container">
+      <div class="searchbar">
+        <Searchbar />
+      </div>
+      <div class="content">
+        <RouterView />
+      </div>
+    </div>
   </div>
-
-  <RouterView />
+  
 </template>
 
 <style scoped>
-/* nav {
-  float: right
-} */
+.container {
+  background-color: #1c1b17;
+  display: flex;
+  height: 100vh;
+}
+
+.navigation {
+  flex-basis: 10%;
+  background-color: lightgray;
+  padding: 20px;
+  box-sizing: border-box;
+}
+
+.content-container {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+}
+.content {
+  flex-basis: 90%;
+  background-color: #1c1b17;
+  color: aquamarine;
+  padding: 20px;
+  box-sizing: border-box;
+  margin: auto;
+}
+
+.searchbar {
+  margin: auto;
+}
 </style>
---
+++ src/components/Searchbar.vue
@@ -0,0 +1,7 @@
+<template>
+  <input type="text" placeholder="Search for pics..." />
+</template>
+
+<script>
+  export default {}
+</script>
