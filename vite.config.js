382008faed7b8ad610ad699a5831e7092fd13889
Joonas Arola <joonas.arola@tuni.fi>
2023-09-19 11:02:35 +0300

    fix vite.config to point at the correct repository

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,6 @@ export default defineConfig({
     }
   },
   publicPath: process.env.NODE_ENV === 'production'
-      ? '/PicFeed/'
+      ? '/https://jonesiii.github.io/PicFeed//'
       : '/'
 })
